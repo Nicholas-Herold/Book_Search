12ac3060e39268f3cf4db2b45f8acad409fe748c
Nicholas Herold <nherold86@gmail.com>
2021-09-30 21:08:49 -0400

    Added apollo and qraph ql, modified server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const path = require('path');
 const {ApolloServer} = require ('apollo-server-express')
 const db = require('./config/connection');
+const {typeDefs, resolvers} = require ('./schemas')
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,5 +25,5 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`));
 });
